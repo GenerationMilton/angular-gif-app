2826f75d9295bbdf52b8e78f44419b72d912ad80
Milton Munoz <livemilton@gmail.com>
2025-05-30 21:45:22 -0500

    angular :sparkles: components for menu

---
+++ src/app/gifs/components/gifs-side-menu-options/gifs-side-menu-options.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'gifs-side-menu-options',
+  imports: [RouterOutlet],
+  templateUrl: './gifs-side-menu-options.component.html',
+
+})
+export default class GifsSideMenuOptionsComponent { }
