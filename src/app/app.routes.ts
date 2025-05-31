49599a20bb2000a61a833459745312568d715552
Milton Munoz <livemilton@gmail.com>
2025-05-30 20:56:45 -0500

    angular :sparkles: thinking in components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path:'dashboard',
+        loadComponent:()=> import('./gifs/page/dashboard-page/dashboard-page.component'),
+        
+    },
+    {
+        path:'trending',
+        loadComponent:()=> import('./gifs/page/trending-page/trending-page.component')
+    },
+    {
+        path:'search',
+        loadComponent:()=> import('./gifs/page/search-page/search-page.component')
+    },
+    {
+        path:'**',
+        redirectTo: 'dashboard'
+    },
+
+
+];
