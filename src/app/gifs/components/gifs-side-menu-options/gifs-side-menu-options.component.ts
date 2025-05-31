c7a7fb488b4bfcbe38df705a5d8be6ef51882a33
Milton Munoz <livemilton@gmail.com>
2025-05-30 22:14:34 -0500

    angular :sparkles: RouterLinks from children components

--- src/app/gifs/components/gifs-side-menu-options/gifs-side-menu-options.component.ts
+++ src/app/gifs/components/gifs-side-menu-options/gifs-side-menu-options.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
+interface MenuOption{
+  icon: string;
+  label: string;
+  route: string;
+  subLabel: string;
+}
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './gifs-side-menu-options.component.html',
 
 })
-export default class GifsSideMenuOptionsComponent { }
+export default class GifsSideMenuOptionsComponent {
+
+    menuOptions: MenuOption[]=[
+      {
+        icon: 'fa-solid fa-chart-line',
+        label: 'Trending',
+        subLabel: 'Gifs Populares',
+        route: '/dashboard/trending',
+      },
+      {
+        icon: 'fa-solid fa-magnifying-glass',
+        label: 'Buscador',
+        subLabel: 'Buscar gifs',
+        route: '/dashboard/search',
+      },
+    ];
+ }
