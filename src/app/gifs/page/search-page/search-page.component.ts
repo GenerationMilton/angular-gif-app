8a31936bd7db85379786ec546a5ce69f4c23c467
Milton Munoz <livemilton@gmail.com>
2025-06-01 19:43:26 -0500

    angular :sparkles: giphy search feature

--- src/app/gifs/page/search-page/search-page.component.ts
+++ src/app/gifs/page/search-page/search-page.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import GifsListComponent from "../../components/gifs-list/gifs-list.component";
+import { GifService } from '../../services/gifs.service';
 
 @Component({
   selector: 'app-search-page',
-  imports: [],
+  imports: [GifsListComponent],
   templateUrl: './search-page.component.html',
 
 })
-export default class SearchPageComponent { }
+export default class SearchPageComponent {
+
+    gifService = inject(GifService);
+
+  onSearch(query: string){
+    this.gifService.searchGifs(query);
+  }
+
+ 
+
+ }
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -4,6 +4,7 @@ import { environment } from '@environments/environment';
 import type { GiphyResponse } from '../interface/giphy.interfaces';
 import { Gif } from '../interface/gif.interfaces';
 import { GifMapper } from '../mapper/gif.mapper';
+import { SearchResponse } from '../interface/search.interfaces';
 
 @Injectable({providedIn: 'root'})
 export class GifService {
@@ -30,6 +31,21 @@ export class GifService {
             this.trendingGifsLoading.set(false);
             console.log({ gifs });
 
+        })
+    }
+
+    searchGifs(query: string){
+        this.http.get<SearchResponse>(`${environment.giphyUrl}/gifs/search`,{
+            params:{
+                api_key: environment.apiKey,
+                q: query,
+                limit:20,
+                offset: 0,
+
+            }
+        }).subscribe((resp)=>{
+            const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
+            console.log({ search: gifs });
         })
     }
    
